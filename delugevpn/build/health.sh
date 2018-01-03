2558011108f41df0b567f3a7bcb5f17c967c5af5
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2018-01-03 09:14:46 +0100

    eppic fail Shaun

--- delugevpn/build/health.sh
+++ delugevpn/build/health.sh
@@ -4,9 +4,9 @@ RES=$(curl -f -m 10 https://ifconfig.co/json)
 WORKING=$(echo $RES | grep ipredator.se)
 
 if [ -z "$WORKING" ]; then
-echo "FAIL" > /health.log
+echo "FAIL" >> /health.log
 exit 1
 else
-echo "OK" > /health.log
+echo "OK" >> /health.log
 exit 0
 fi
